190a1941aa0f34723be07c5d6e25615f381a6940
eugeneborodin <eugeneborodin@mail.ru>
2017-02-06 01:38:46 +0300

    init

---
+++ AOUT.CH3.LogAnEvent.UnitTests/EventRelatedTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+using NSubstitute;
+
+namespace AOUT.CH3.LogAnEvent.UnitTests
+{
+    [TestFixture]
+    public class EventRelatedTests
+    {
+        [Test]
+        public void ctor_WhenViewIsLoaded_CallsViewRender()
+        {
+            var mockView = Substitute.For<IView>();
+
+            Presenter p = new Presenter(mockView);
+            mockView.Loaded += Raise.Event<Action>();
+            mockView.Received().Render(Arg.Is<string>(s => s.Contains("Hello World")));
+        }
+    }
+}
---
+++ AOUT.CH3.LogAnEvent/Presenter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOUT.CH3.LogAnEvent
+{
+    public class Presenter
+    {
+        private readonly IView _view;
+
+        public Presenter(IView view)
+        {
+            _view = view;
+            this._view.Loaded += OnLoaded;
+        }
+
+        private void OnLoaded()
+        {
+            _view.Render("Hello World");
+        }
+    }
+
+    public interface IView
+    {
+        event Action Loaded;
+        void Render(string text);
+    }
+}
